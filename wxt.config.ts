36ffff0e727ea761abd100925ce006fe240e0a15
MarcosTacos <marcantoine.carrier@hotmail.com>
2025-02-28 13:27:40 -0500

    First commit

---
+++ wxt.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  extensionApi: 'chrome',
+  srcDir: 'src',
+  runner: {
+    binaries: {
+    chrome: '/path/to/chrome-beta', // Use Chrome Beta instead of regular Chrome
+    firefox: 'firefoxdeveloperedition', // Use Firefox Developer Edition instead of regular Firefox
+    edge: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', // Open MS Edge when running "wxt -b edge"
+    },
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+},
+  modules: ['@wxt-dev/module-react'],
+});
